669f27aaef550ba9b76398f1ecf3f6617f66b092
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-03 13:53:18 +0900

    refactor: ホスト名の許可リストの設定は不要なので削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,8 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << '946f2c5b39434943af6f75e1da8fc2b8.vfs.cloud9.us-east-1.amazonaws.com'
-
   config.after_initialize do
     Bullet.enable = true
     Bullet.alert = false

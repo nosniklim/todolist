49cddd43c65df987c00da97541daa37bcd213565
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-03 12:28:10 +0900

    refactor: Railsが自動生成したファイルに差し替え - config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,6 +18,5 @@ module RailsTodo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoload_paths += %W[#{config.root}/lib]
   end
 end

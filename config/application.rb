d8feff7a8b4fb63c4552a17397683318927b7137
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-04 12:30:39 +0900

    fix: lint error

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module RailsTodo
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join("lib")
   end
 end

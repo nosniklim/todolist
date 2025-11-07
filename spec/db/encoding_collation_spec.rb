58d238593c382f9b4dd2e7881e958fe0fb1f7247
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-07 21:59:19 +0900

    fix: rubocop error

--- spec/db/encoding_collation_spec.rb
+++ spec/db/encoding_collation_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'MySQL charset & collation' do
   it 'database uses utf8mb4 + utf8mb4_0900_ai_ci' do
     db = ActiveRecord::Base.connection.exec_query(
-      "SELECT @@character_set_database AS cs, @@collation_database AS col"
+      'SELECT @@character_set_database AS cs, @@collation_database AS col'
     ).to_a.first
     expect(db['cs']).to eq('utf8mb4')
     expect(db['col']).to eq('utf8mb4_0900_ai_ci')

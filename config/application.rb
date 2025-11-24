5cdcaaf1ed77a273dd8337320d2805c8d8e676e7
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-24 14:49:56 +0900

    refactor: Rails7.2が自動生成したファイルに差し替え - config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,10 @@ module RailsTodo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Set the cache format version
-    config.active_support.cache_format_version = 7.1
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -25,8 +22,6 @@ module RailsTodo
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-
-    # NOTE: Rails7.1
-    config.add_autoload_paths_to_load_path = false
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end

008dd021b52a5cb082d139eda76453e4b2ad1518
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-24 16:00:32 +0900

    feat: add new rails default #212 - config.active_job.queue_adapter

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
+  # NOTE: Rails7.2
+  config.active_job.queue_adapter = :test
+
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true

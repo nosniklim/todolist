5676cd7440baa1be47deb2dea8ad7591b0b2bb24
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-24 20:05:25 +0900

    chore: Rails7.1で追加したautoload_pathsの設定をrevert

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RailsTodo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # NOTE: Rails7.1
+    config.add_autoload_paths_to_load_path = false
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.

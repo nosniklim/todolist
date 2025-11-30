cc1b1d85d4df1d02477cb971352bb84af4213321
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-30 17:48:34 +0900

    refactor: webpackをpacksから移動して正規のエントリに統合 - app/javascript/application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,17 @@
 // Entry point for the build script in your package.json
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import './channels/index.js';
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();

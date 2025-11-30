6315d8780c9fe0c596c67fb25160324602fcb432
nosniklim <nosniklim_engineer@outlook.jp>
2025-11-30 18:57:28 +0900

    chore: webpack.config.cjsにES出力設定を追加

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,7 +11,13 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
+    // ESモジュール形式で出力
+    // `type="module"` のスクリプトとしてブラウザへ渡すための設定（`chunkFormat: "module"` を使う場合に必要）
+    module: true,
     path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  experiments: {
+    outputModule: true,
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({

b17e3a5037c86594c299ad3ffe021cf9f229d441
nosniklim <nosniklim_engineer@outlook.jp>
2025-12-06 23:09:54 +0900

    chore: bundle exec annotate

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :bigint           not null, primary key
+#  position   :integer          default(0), not null
+#  title      :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
 FactoryBot.define do
   factory :list do
     title { 'Sample List' }

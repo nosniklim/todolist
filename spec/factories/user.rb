a335199229e7fd38f72087c14d4de75864269fb5
nosniklim <nosniklim_engineer@outlook.jp>
2025-12-07 00:14:35 +0900

    chore: spec/factories/users.rbのファイル名変更

--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { Faker::Internet.unique.username(specifier: 2..20) }
-    email { Faker::Internet.unique.email }
-    password { 'password' }
-    password_confirmation { 'password' }
-  end
-end

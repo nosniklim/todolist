e174d869b5cd655699386676360173782dd2e6ac
nosniklim <nosniklim_engineer@outlook.jp>
2025-12-28 16:49:36 +0900

    feat: API用のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  namespace :api do
+    namespace :v1 do
+      resources :lists, only: [:index] do
+        collection do
+          patch :reorder
+        end
+      end
+    end
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'

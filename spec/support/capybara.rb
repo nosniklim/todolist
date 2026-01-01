3f47aa5f10e20f17ab7f0f752464ffb6c3bde685
nosniklim <nosniklim_engineer@outlook.jp>
2026-01-01 14:09:06 +0900

    remove: disable-gpu option in capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,7 +11,6 @@ Capybara.save_path = 'tmp/capybara'
 Capybara.register_driver :selenium_remote_chrome do |app|
   chrome_options = Selenium::WebDriver::Chrome::Options.new
   chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--disable-gpu')
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--disable-dev-shm-usage')
   chrome_options.add_argument('--window-size=1920,1080')

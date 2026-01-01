59a5664c0711d3f78970e0e62a5d77bf9b0c458e
nosniklim <nosniklim_engineer@outlook.jp>
2026-01-01 14:08:07 +0900

    fix: chrome_optionsの指定方法

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -10,11 +10,11 @@ Capybara.save_path = 'tmp/capybara'
 # Seleniumの設定を登録
 Capybara.register_driver :selenium_remote_chrome do |app|
   chrome_options = Selenium::WebDriver::Chrome::Options.new
-  chrome_options.add_argument('headless')
-  chrome_options.add_argument('disable-gpu')
-  chrome_options.add_argument('no-sandbox')
-  chrome_options.add_argument('disable-dev-shm-usage')
-  chrome_options.add_argument('window-size=1920,1080')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-gpu')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.add_argument('--window-size=1920,1080')
   Capybara::Selenium::Driver.new(app,
                                  browser: :remote,
                                  url: "http://#{ENV.fetch('SELENIUM_HOST', 'localhost')}:4444/wd/hub",
